911e336db69539bc9cb65cad9cb8ac48d5286103
Lee-Gwang-soo <59168432+Lee-Gwang-soo@users.noreply.github.com>
2021-03-13 19:24:54 +0900

    ionic-react UI와 state 등이 세팅된 App.js

--- src/App.js
+++ src/App.js
@@ -1,49 +1,63 @@
-import { 
-  IonApp,
-  IonButton, 
-  IonContent, 
-  IonHeader, 
-  IonIcon, 
-  IonTitle, 
-  IonToast, 
-  IonToolbar
-} from '@ionic/react'
-import React, {useState} from 'react';
-import { play as playIcon } from 'ionicons/icons'
+import {
+	IonApp,
+	IonContent,
+	IonHeader,
+	IonTitle,
+	IonToolbar,
+	IonLabel,
+	IonItem,
+	IonInput,
+	IonDatetime
+} from '@ionic/react';
+import React, { useState } from 'react';
+import BiorhythmCard from './component/BiorhythmCard';
 
 function App() {
-  const [showToast, setShowToast] = useState(false);
-  // IonToast 기능중에 open값으로 true와 false를 줄 수 있다.  
-  const handleClick= () => {
-    setShowToast(true)
-    setTimeout(()=> setShowToast(false), 1500);
-  }
-  return (
-    <IonApp> 
-      <IonHeader>
-        <IonToolbar>  
-        <IonTitle>My App</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        {/* Ionbutton은 color 속성있음 (primary, secondary, warning 등등)
-        color 속성값들 : https://ionicframework.com/docs/theming/basics
-        IonIcon에는 slot 속성이 있음. (start , end, icon-only) */}
-        <IonButton color="primary" onClick={handleClick}>
-        <IonIcon icon={playIcon} slot="start"/>
-          Click Me
-        </IonButton>
-        <IonButton color="danger" onClick={handleClick}>
-        <IonIcon icon={playIcon} slot="end"/>
-          Click Me
-        </IonButton>
-        <IonButton color="warning" onClick={handleClick}>
-        <IonIcon icon={playIcon} slot="icon-only"/>
-        </IonButton>
-        <IonToast isOpen={showToast} message="Hello world!" />
-      </IonContent>
-    </IonApp>
-  );
+	const [ name, setName ] = useState('');
+	const [ birthDate, setBirthDate ] = useState('');
+	const [ targetDate, setTargetDate ] = useState(new Date().toISOString());
+
+	return (
+		<IonApp>
+			<IonHeader>
+				<IonToolbar>
+					<IonTitle>BioRhythms</IonTitle>
+				</IonToolbar>
+			</IonHeader>
+			<IonContent className="ion-padding">
+				{/* claaName에 "ion-padding"이라는 Name을 지정해주면 Ionic에 지정된 css가 적용된다. */}
+				{birthDate && <BiorhythmCard name={name} birthDate={birthDate} targetDate={targetDate} />}
+				<IonItem>
+					<IonLabel position="stacked">Name : </IonLabel>
+					{/* IonLabel에는 position이라는 속성값이 있다. fixed(모든 라벨의 영역을 같은값으로 고정), floating(일반적인 label에 애니메이션동작 추가), stacked(처음부터 애니메이션동작) */}
+					<IonInput
+						type="text"
+						placeholder="Your Name"
+						value={name}
+						onIonChange={(event) => setName(event.detail.value)}
+					/>
+					{/* IonInput에서는 React와 달리 onChange가 아닌 onIonChange함수를 써야하고, event.target 대신에 event.detail을 써야한다 */}
+				</IonItem>
+				<IonItem>
+					<IonLabel position="stacked">Birth : </IonLabel>
+					<IonDatetime
+						displayFormat="YYYY-MM-DD(DDDD)"
+						value={birthDate}
+						onIonChange={(event) => setBirthDate(event.detail.value)}
+					/>
+					{/* IonDateTime은 날짜를 선택하게 해주는 라이브러리, displayFormat으로 입력값을 custom할 수 있다. */}
+				</IonItem>
+				<IonItem>
+					<IonLabel position="stacked">Target : </IonLabel>
+					<IonDatetime
+						displayFormat="YYYY-MM-DD(DDDD)"
+						value={targetDate}
+						onIonChange={(event) => setTargetDate(event.detail.value)}
+					/>
+				</IonItem>
+			</IonContent>
+		</IonApp>
+	);
 }
 
 export default App;
